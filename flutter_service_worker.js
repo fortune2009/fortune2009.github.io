a5e3273f9871da073a6829ab5f213c0e6c50d1fa
fortune2009 <your-email@example.com>
2025-10-24 15:15:22 +0100

    deploy: final clean deployment with proper assets structure

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "92ce874d70d56caf9939b84341180962",
+const RESOURCES = {"flutter_bootstrap.js": "0f18190dbe516207785efe11838d32e6",
 "version.json": "d64d9b8b75680a45c5ca3a13dbcd3d57",
 "index.html": "3af7e08d10b11a772e1eccf78c7d7d9d",
 "/": "3af7e08d10b11a772e1eccf78c7d7d9d",
